97dc2560310a959a868acc9c6d28c95a48cbca31
b1ew <yogafishguts@gmail.com>
2023-06-26 13:14:16 +0700

    build: zustand

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,6 +4,11 @@ import { DatePickerProps } from './DatePicker';
 interface EnhanceDatePickerProps extends Pick<DatePickerProps, 'onCancel'> {
     isOpen?: boolean;
 }
+export declare type DateState = {
+    value: Date;
+    setValue: (value: Date) => void;
+};
+export declare const useStoreDate: import("zustand").UseBoundStore<import("zustand").StoreApi<DateState>>;
 declare type ModalDatePickerProps = EnhanceDatePickerProps & DatePickerProps & {
     isPopup?: boolean;
 };
